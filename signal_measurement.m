cc00e029840067f0ec436e7922d987750e3ca7d9
Karlo Sever <severkarlo336@gmail.com>
2019-06-17 11:39:34 +0200

    Measurement & reconstruction based on PCA

--- signal_measurement.m
+++ signal_measurement.m
@@ -22,15 +22,15 @@ hold off;
 
 %% Mjerenje signala - 15 signala razlicitih faznih pomaka za svaku od 15 frekvencija
 
-f = linspace(5e6, 10e6, 15);            % 15 frekvencija od 5 MHz do 10 MHz
+f = linspace(3e6, 5e6, 15);            % 15 frekvencija od 5 MHz do 10 MHz
 faza = linspace(0, 2*pi, 15);           % 15 faznih pomaka od 0 rad do 2pi rad
 t = 0:1e-8:1e-6;
 
 X = zeros(15,101,15);                   % frekvencija x vrijeme x faza
 C = zeros(15,101,15);
 
-for i = 1:15                            % iterator frekvencije
-    for j = 1:15                        % iterator faze
+for i = 1:size(C, 1)                            % iterator frekvencije
+    for j = 1:size(C, 3)                        % iterator faze
         X(i,:,j) = awgn(sin(2*pi*f(i)*t + faza(j)), 20);
         for k = 1:10:101
             C(i,k,j) = X(i,k,j);
@@ -40,8 +40,8 @@ end
 
 %% Mjerenje signala - prikaz rezultata mjerenja 15x15 (225) signala
 
-for i = 1:15
-    for j = 1:15
+for i = 1:size(C, 1)
+    for j = 1:size(C, 3)
         plot(t, X(i,:,j));
         hold on
         stem(t, C(i,:,j));
@@ -50,7 +50,7 @@ for i = 1:15
         xlabel('Vremenski osdjecak 1us');
         title('Signal i deset uzoraka na odsjecku');
         pause;                                                   % iteracija se nastavlja s ENTER, prekida s CTRL+C
-        msg = sprintf('Frekvencija: %f | Faza: %f', f(i), faza(j));
+        msg = sprintf('Frekvencija: %f MHz | Faza: %f rad', f(i)/1e6, faza(j));
         disp(msg);
     end
 end
@@ -58,7 +58,7 @@ end
 %% Kovarijancijska matrica
 % Za svaki skup signala jedne frekvencije izracunaj mean (mu) i kov. matricu (sigma)
 
-for i = 1:15                % iterator frekvencije
+for i = 1:size(C,1)                % iterator frekvencije
 
     sigma(:,:,i) = zeros(length(t), length(t));
     mu(:,i) = mean(C(i,:,:), 3);
@@ -94,7 +94,27 @@ phi = (V_un(:, end-M+1:end)');
 
 %% Rekonstrukcija
 
-
+for i = 1:size(C, 1)                    % iterator frekvencije
+    for k = 1:size(C, 3)                % iterator faze
+       
+        C_test = C(i,:,k);
+        y = phi*C_test';
+        X_rec(:,:,k) = phi'*y;
+        %alfa_rec(:,:,k) = V(:,:,k)'*X_rec(:,:,k);
+        
+        C_rec(:,:,k) = phi'*y;
+        alfa_rec(:,:,k) = V(:,:,k)'*X_rec(:,:,k);
+        sig_rec = V(:,:,k)* alfa_rec(:,:,k);
+        
+        stem(t, C_test);
+        hold on;
+        stem(t, sig_rec);
+        hold off;
+        xlabel('Vremenski osdjecak 1us');
+        title('Signal i deset uzoraka na odsjecku');
+        pause;                          % iteracija se nastavlja s ENTER, prekida s CTRL+C
+    end
+end
 
 
 
